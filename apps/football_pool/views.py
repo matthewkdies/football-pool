f7bacf8eff47830f93f99a7587fa7a9f6c2c0de6
Matt Dies <matthewkdies@gmail.com>
2024-08-30 04:43:12 +0000

    Correcting templates dir.

--- apps/football_pool/views.py
+++ apps/football_pool/views.py
@@ -1,8 +1,10 @@
 # keep this file even if you don't need traditional views
 # as it holds the blueprint app instance
+from pathlib import Path
+
 from flask import Blueprint, flash, redirect, render_template, url_for
 
-app = Blueprint('football_pool', __name__, template_folder='templates')
+app = Blueprint('football_pool', __name__, template_folder=Path(__file__).parents[2]/ "templates")
 
 @app.route('/')
 def hello_world():
