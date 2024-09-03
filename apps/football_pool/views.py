be2b09c58137106ec78fdf4746084d13d8624476
Matt Dies <matthewkdies@gmail.com>
2024-09-03 01:47:45 +0000

    Rules placeholder page.

--- apps/football_pool/views.py
+++ apps/football_pool/views.py
@@ -35,6 +35,11 @@ def about():
     return render_template("about.html")
 
 
+@app_blueprint.route("/rules")
+def rules():
+    return render_template("rules.html")
+
+
 @app_blueprint.route("/test")
 def test():
     teams: list[Team] = Team.query.all()
