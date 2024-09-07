3501e887095c2c545d706587c8e554c9e952e1b6
Matt Dies <matthewkdies@gmail.com>
2024-09-07 02:16:01 +0000

    Fixing calculation to remove games that haven't begun.

--- apps/football_pool/get_scores/make_games.py
+++ apps/football_pool/get_scores/make_games.py
@@ -26,7 +26,9 @@ def find_teams_with_most_points(current_week: CurrentWeek) -> list[Team]:
     Returns:
         list[Team]: A list of Team objects that scored the most points.
     """
-    most_points = max(game.max_score for game in current_week.games)
+    most_points = max(
+        game.max_score for game in current_week.games if game.status != GameStatus.QUEUED
+    )
     logger.debug("The most points scored in the week is %s.", most_points)
     winning_teams: list[Team] = []
     for game in current_week.games:
@@ -47,7 +49,9 @@ def find_teams_with_least_points(current_week: CurrentWeek) -> list[Team]:
     Returns:
         list[Team]: A list of Team objects that scored the least points.
     """
-    least_points = min(game.min_score for game in current_week.games)
+    least_points = min(
+        game.min_score for game in current_week.games if game.status != GameStatus.QUEUED
+    )
     logger.debug("The least points scored in the week is %s.", least_points)
     winning_teams: list[Team] = []
     for game in current_week.games:
