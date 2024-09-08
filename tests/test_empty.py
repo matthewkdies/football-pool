8ff0d6cdf01ef13c3ff52ba665a896dcff0e0b8f
Matt Dies <matthewkdies@gmail.com>
2024-09-08 23:53:26 +0000

    Removing old test.

--- tests/test_empty.py
+++ tests/test_empty.py
@@ -1,20 +1,9 @@
-# collection of tests to assert flask-empty works properly
+"""This is placeholder file in the event I ever decide to write tests. But I won't. So."""
 
-from apps.football_pool.main import App
+from apps.football_pool import create_app
+
+app = create_app()
 
 
-def test_valid_json_():
-    from json import dumps
-
-    py_dict = {"foo": True, "bar": False, "zex": None}
-    json_dict = {"foo": "true", "bar": "false", "zex": "null"}
-
-    # equivalent
-    assert dumps(py_dict) == dumps(json_dict)
-
-    # symmetric
-    for key, value in py_dict.items():
-        assert json_dict.get(key) == value
-
-    for key, value in json_dict.items():
-        assert py_dict.get(key) == value
+def test_foo():
+    assert "foo bar".title() == "Foo Bar"
