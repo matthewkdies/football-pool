c24c5237c69ca87e327f8febc617a4e517eff9b6
Matt Dies <matthewkdies@gmail.com>
2025-08-15 16:30:42 +0000

    Added script to build, push, and clean images.

---
+++ prod/build_and_push_image.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+set -e
+
+LOCAL_IMAGE="football-pool-local-build:latest"
+REMOTE_IMAGE_NAME="matthewkdies/football-pool"
+DATE_TAG=$(date +%d%b%g)
+
+docker build --file "${GIT_DIR}/football-pool/prod/prod.dockerfile" --tag "${LOCAL_IMAGE}" "${GIT_DIR}/football-pool"
+docker tag "${LOCAL_IMAGE}" "${REMOTE_IMAGE_NAME}:latest"
+docker tag "${LOCAL_IMAGE}" "${REMOTE_IMAGE_NAME}:${DATE_TAG}"
+docker push "${REMOTE_IMAGE_NAME}:latest"
+docker push "${REMOTE_IMAGE_NAME}:${DATE_TAG}"
+docker image rm "${LOCAL_IMAGE}"
+docker image rm "${REMOTE_IMAGE_NAME}:latest"
+docker image rm "${REMOTE_IMAGE_NAME}:${DATE_TAG}"
