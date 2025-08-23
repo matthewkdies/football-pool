84e9882a3176a59490071c30f14e416cf8250414
Matt Dies <matthewkdies@gmail.com>
2025-08-23 02:22:46 +0000

    Ditto

--- prod/entrypoint.sh
+++ prod/entrypoint.sh
@@ -11,21 +11,12 @@ fi
 # change UID and GID as needed
 OLD_UID=$(id -u "${USER_NAME}")
 OLD_GID=$(id -g "${USER_NAME}")
-NEW_USERNAME="${NEW_USERNAME:-new_user}"
-NEW_GROUPNAME="${NEW_GROUPNAME:-new_group}"
 if [[ "${OLD_UID}" != "${PUID}" || "${OLD_GID}" != "${PGID}" ]]; then
-    echo "INFO: Creating group, if it doesn't exist already."
-    getent group "${NEW_GROUPNAME}" &>/dev/null || groupadd --gid "${PGID}" "${NEW_GROUPNAME}"
-
-    echo "INFO: Creating user, if it doesn't exist already."
-    id -u "${NEW_USERNAME}" &>/dev/null || useradd --uid "${PUID}" --gid "${PGID}" "${NEW_USERNAME}"
-
-    echo "INFO: Changing IDs of user to (${OLD_UID}:${OLD_GID}) to ${PUID}:${PGID}."
-    groupmod --gid "${PGID}" "${NEW_GROUPNAME}"
-    usermod --uid "${PUID}" "${NEW_USERNAME}"
-    find / -user "${OLD_UID}" -exec echo chown "${PUID}" '{}' \; 2>/dev/null
-    find / -user "${OLD_UID}" -exec chown "${PUID}:${PGID}" {} + 2>/dev/null
-    find / -group "${OLD_GID}" -exec chown "${PUID}:${PGID}" {} + 2>/dev/null
+    echo "INFO: Changing IDs of user from ${OLD_UID}:${OLD_GID} to ${PUID}:${PGID}."
+    groupmod --gid "${PGID}" "${USER_NAME}" || echo "No GID changes necessary."
+    usermod --uid "${PUID}" "${USER_NAME}" || echo "No UID changes necessary."
+    find / -user "${OLD_UID}" -exec chown "${PUID}" {} + 2>/dev/null
+    find / -group "${OLD_GID}" -exec chown "${PGID}" {} + 2>/dev/null
 fi
 
 # migrate the database if needed
